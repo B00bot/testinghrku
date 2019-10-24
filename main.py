59162c3819abf835553544702e0146ac4256601a
B00bot <55877415+B00bot@users.noreply.github.com>
2019-10-24 10:41:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,6 @@ from telebot import types
 import os
 import logging
 import random
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                     level=logging.INFO)
-PORT = int(os.environ.get('PORT', '8443'))
 bot = telebot.TeleBot(config.token)
 
 markup = types.ReplyKeyboardMarkup()
@@ -19,6 +16,8 @@ button4 = types.KeyboardButton('Памагити')
 markup.row(button1, button2, button3)
 markup.row(button4)
 
+#stickers for random
+stickers = ["CAADAgADCwADlp-MDpuVH3sws_a7FgQ", "CAADAgAD7g0AAqgILwj_8DhBu2dnDRYE", "CAADBAADfQADzjkIDSgZQLclD7jiFgQ", "CAADBAADRAADzjkIDbv4-ULKD6hiFgQ", "CAADAgAD0gIAArnzlwt4AXAE0tVijhYE", "CAADAgAD2gEAAsdjXBUX3pc5V_GYDBYE", "CAADBAADmAADzjkIDRaa2RCZbCJWFgQ", "CAADBAADkwADzjkIDYydFNXPYxHoFgQ", "CAADAgAD4w0AAqgILwh6UH_uBQWn_RYE", "CAADAgADBAgAAhhC7ghzMDDTpZ3HjRYE", "CAADAgADCAADl_TGFHTucAABYtoR1BYE"]
 
 @bot.message_handler(commands=['start'])
 def start(message):
@@ -27,32 +26,29 @@ def start(message):
 @bot.message_handler(content_types=['text'])
 def secret(message):
     if message.text.lower()="секрет"
-        bot.sendMessage(chat_id=update.message.chat_id, text="Мой создатель любит Лапу", reply_markup=markup)
+        bot.sendMessage(chat_id=message.chat_id, text="Мой создатель любит Лапу", reply_markup=markup)
 
 
 @bot.message_handler(content_types=['text'])
 def help(message):
     if message.text.lower()="памагити":
-        bot.sendMessage(chat_id=update.message.chat_id, text="если хочешь узнать тайну, Нажми Секрет Если нужны доказательства - нажми Доказательство. Если грустно - нажми Грустно, если нужна помошь, нажми Памагити", reply_markup=markup)	
+        bot.sendMessage(chat_id=message.chat_id, text="если хочешь узнать тайну, Нажми Секрет Если нужны доказательства - нажми Доказательство. Если грустно - нажми Грустно, если нужна помошь, нажми Памагити", reply_markup=markup)	
 
 
-#stickers for random
-stickers = ["CAADAgADCwADlp-MDpuVH3sws_a7FgQ", "CAADAgAD7g0AAqgILwj_8DhBu2dnDRYE", "CAADBAADfQADzjkIDSgZQLclD7jiFgQ", "CAADBAADRAADzjkIDbv4-ULKD6hiFgQ", "CAADAgAD0gIAArnzlwt4AXAE0tVijhYE", "CAADAgAD2gEAAsdjXBUX3pc5V_GYDBYE", "CAADBAADmAADzjkIDRaa2RCZbCJWFgQ", "CAADBAADkwADzjkIDYydFNXPYxHoFgQ", "CAADAgAD4w0AAqgILwh6UH_uBQWn_RYE", "CAADAgADBAgAAhhC7ghzMDDTpZ3HjRYE", "CAADAgADCAADl_TGFHTucAABYtoR1BYE"]
 
 @bot.message_handler(content_types=['text'])
 def proof(message):
     if message.text.lower()="доказательство":
         randomstick=random.randint(0,10)
         pic=stickers[randomstick]
-        bot.sendSticker(chat_id=update.message.chat_id, sticker=pic, reply_markup=markup);
+        bot.sendSticker(chat_id=message.chat_id, sticker=pic, reply_markup=markup);
 
 @bot.message_handler(content_types=['text'])
 def grustno(message):
     if message.text.lower()="памагити":
         pic=open('s1200.jpeg', 'rb')
-        bot.send_photo(chat_id=update.message.chat_id, photo=pic);
-        bot.sendMessage(chat_id=update.message.chat_id, text="Ни грустииии", reply_markup=markup)
+        bot.send_photo(chat_id=message.chat_id, photo=pic);
+        bot.sendMessage(chat_id=message.chat_id, text="Ни грустииии", reply_markup=markup)
 
 
 bot.infinity_polling(True)
-bot.idle() 
