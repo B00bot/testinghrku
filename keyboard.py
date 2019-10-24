553fdb0771c2361354d841d87668f8a975022a14
B00bot <55877415+B00bot@users.noreply.github.com>
2019-10-24 18:46:20 +0300

    Update keyboard.py

--- keyboard.py
+++ keyboard.py
@@ -2,5 +2,5 @@ import telebot
 from telebot import types
 
 keyb = types.ReplyKeyboardMarkup(True, False)
-keyb.row('Секрет', 'Докажи','Грустно')
-keyb.row('Нипанятнаа')
+keyb.row('❤️Секрет❤️', '😘 Докажи 😘','😞 Грустно' 😞)
+keyb.row('🤷‍♂️Нипанятнаа🤷‍♂️')
