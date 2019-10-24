5290daa051f5d41e3e95ac1baf43f5273f33c37d
B00bot <55877415+B00bot@users.noreply.github.com>
2019-10-24 16:36:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,45 +1,58 @@
-from config import TOKEN
-import telebot
 import os
-from flask import Flask, request
+
+import telebot
 from config import *
- 
- 
- 
- 
+from config import TOKEN
+from flask import Flask, request
+import random
+
 bot = telebot.TeleBot(TOKEN)
 server = Flask(__name__)
- 
- 
- 
+
+stickers = ["CAADAgADCwADlp-MDpuVH3sws_a7FgQ", "CAADAgAD7g0AAqgILwj_8DhBu2dnDRYE", "CAADBAADfQADzjkIDSgZQLclD7jiFgQ", "CAADBAADRAADzjkIDbv4-ULKD6hiFgQ", "CAADAgAD0gIAArnzlwt4AXAE0tVijhYE", "CAADAgAD2gEAAsdjXBUX3pc5V_GYDBYE", "CAADBAADmAADzjkIDRaa2RCZbCJWFgQ", "CAADBAADkwADzjkIDYydFNXPYxHoFgQ", "CAADAgAD4w0AAqgILwh6UH_uBQWn_RYE", "CAADAgADBAgAAhhC7ghzMDDTpZ3HjRYE", "CAADAgADCAADl_TGFHTucAABYtoR1BYE"]
+
+
 @bot.message_handler(commands=['start'])
 def start_message(msg):
-    bot.send_message(msg.chat.id, f'Даров {msg.from_user.first_name}')
- 
- 
-@bot.message_handler(func=lambda m: True)
-def echo_all(msg):
-    bot.reply_to(msg, msg.text)
- 
- 
- 
- 
- 
- 
+    bot.send_message(msg.chat.id, "Привет, если хочешь узнать тайну, отправь /secret Если нужны доказательства - отправь /proof Если грустно - отправь /grustno")
+
+
+@bot.message_handler(commands=['secret'])
+def secret_message(msg):
+    bot.sendMessage(msg.chat_id, "Мой создатель любит тебя")
+
+@bot.message_handler(commands=['help'])
+def help_message(msg):
+    bot.sendMessage(msg.chat_id, "Если хочешь узнать тайну, отправь /secret Если нужны доказательства - отправь /proof Если грустно - отправь /grustno")
+
+@bot.message_handler(commands=['proof'])
+def proof_message(msg):
+    randomstick = random.randint(0, 10)
+    pic = stickers[randomstick]
+    bot.sendMessage(msg.chat_id, "Создатель просил передать...")
+    bot.sendSticker(msg.chat_id, pic);
+
+@bot.message_handler(commands=['grustno'])
+def grustno(msg):
+    pic=open('s1200.jpeg', 'rb')
+    bot.send_photo(msg.chat_id, pic);
+    bot.sendMessage(msg.chat_id, "Ни грустииии")
+
 @server.route('/' + TOKEN, methods=['POST'])
 def getMessage():
     bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
     return "!", 200
- 
- 
+
+
 @server.route('/', methods=["GET"])
 def index():
     return "Hello from Heroku!", 200
- 
- 
+
+
 if __name__ == '__main__':
     bot.remove_webhook()
     bot.set_webhook(url=f'{URLHEROKU}' + TOKEN)
     print(bot.get_webhook_info().__dict__)
     server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
-    #bot.polling(none_stop=True)
+
+    # bot.polling(none_stop=True)
