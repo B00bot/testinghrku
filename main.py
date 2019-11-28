e766c758b244347a79169fbaae6753d67eaf1f8e
B00bot <55877415+B00bot@users.noreply.github.com>
2019-11-28 23:27:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -105,7 +105,7 @@ def frwrdmess(msg):
                     controler_brain_q = 0.0
                 print(controler_brain_q)
                 if messtext.find('''Коготь 🐶🐈Химеры''') != -1:
-                    chimera_claw_q = float(re.search(r”Коготь 🐶🐈Химеры\s(\d+)\sшт” , messtext).group(1))
+                    chimera_claw_q = float(re.search(r'''Коготь 🐶🐈Химеры\s(\d+)\sшт''' , messtext).group(1))
                     print(chimera_claw_q)
                 else:
                     chimera_claw_q = 0.0
