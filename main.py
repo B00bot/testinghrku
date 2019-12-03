98851691aac1841a8d860f7720b48ccdbfe79c68
B00bot <55877415+B00bot@users.noreply.github.com>
2019-12-03 16:18:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -123,10 +123,12 @@ def frwrdmess(msg):
                     poltergeist_heart_q = float(re.search(r'''Сердце 🔥Полтергейста\s(\d+)\sшт''' , messtext).group(1))
                 else:
                     poltergeist_heart_q = 0.0
+                artefacts = 0.0
                 if messtext.find('''«Пламя»''') != -1:
-                    artefacts_q += float(re.search(r'''«Пламя»\s(\d+)\sшт''' , messtext).group(1))
+                    artefacts = float(re.search(r'''«Пламя»\s(\d+)\sшт''' , messtext).group(1))
+                    artefacts_q += artefacts
                 else:
-                    artefacts_q = artefacts_q + 0.0
+                    artefacts_q = artefacts + 0.0
                 gesheft = (dog_tail_q * 50.0) + (zombie_hand_q * 62.5) + (hog_hoof_q * 100.0) + (sucker_leg_q * 125.0) + (snork_foot_q * 150.0) + (jerboa_head_q * 166.5) + (snork_head_q * 200.0) + (burer_leg_q * 375.0) + (controler_hand_q * 625.0) + (burer_hand_q * 625.0) + (sucker_tentacle_q * 750.0) + (pseudo_giant_q * 750) + ( controler_brain_q * 1250) + (chimera_claw_q * 1250) + (poltergeist_heart_q * 1875.0)
                 gesheft_science = gesheft * 4
                 gesheft_premium_a = gesheft * 1.1
