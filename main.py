cf438cf4ff5f87543758f971eae6bc3b045b8e25
B00bot <55877415+B00bot@users.noreply.github.com>
2019-12-15 21:35:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -23,14 +23,14 @@ def start_msg(msg):
 
 def process_name_step(msg):
     name = msg.text
-    bot.send_message(msg.chat.id, f'''Приятно познакомиться, {name}. Выбери пол героя: M - мужчина, F - женщина''')
+    bot.send_message(msg.chat.id, f'''Приятно познакомиться, {name}. Выбери пол героя: М - мужчина, Ж - женщина''')
     bot.register_next_step_handler(msg, sex_step)
 
 def sex_step(msg):
-    if msg.text.upper == 'M':
-        sex = 'Мужчина'
-    elif msg.text.upper == 'F':
-        sex = 'Женщина'
+    if msg.text.upper == 'М':
+        sex.global = 'Мужчина'
+    elif msg.text.upper == 'Ж':
+        sex.global = 'Женщина'
     else:
         bot.send_message(msg.chat.id, '''Неверный ввод''')     
     bot.send_message(msg.chat.id, f'''Регистрация завершена. Твой персонаж {name}, {sex})''')
