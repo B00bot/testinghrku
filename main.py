81e83da438bb1894b594cdf239b7c7cdec42e44c
B00bot <55877415+B00bot@users.noreply.github.com>
2019-12-15 20:58:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,21 +12,21 @@ bot = telebot.TeleBot(TOKEN)
 server = Flask(__name__)
 
 @bot.message_handler(commands=['start'])
-    def start_msg(msg):
-         bot.send_message(msg.chat.id, f'''Приветствую тебя в карточной игре {Game_name}. Как нам тебя называть?''')
-         bot.register_next_step_handler(msg, process_name_step)
+def start_msg(msg):
+    bot.send_message(msg.chat.id, f'''Приветствую тебя в карточной игре {Game_name}. Как нам тебя называть?''')
+    bot.register_next_step_handler(msg, process_name_step)
 
-    def process_name_step(msg):
-        name = msg.text
-        bot.send_message(msg.chat.id, f''Приятно познакомиться, {name}. Выбери пол героя: М - мужчина, Ж - женщина''')
-        bot.register_next_step_handler(msg, sex_step)
+def process_name_step(msg):
+    name = msg.text
+    bot.send_message(msg.chat.id, f''Приятно познакомиться, {name}. Выбери пол героя: М - мужчина, Ж - женщина''')
+    bot.register_next_step_handler(msg, sex_step)
 
-    def sex_step(msg):
-        if msg.text.lower == 'М':
-            sex = 'Мужчина'
-        elif msg.text.lower == 'Ж':
-            sex = 'Женщина'
-        bot.send_message(msg.chat.id, f'''Регистрация завершена. Твой персонаж {name}, пол - {sex})
+def sex_step(msg):
+    if msg.text.lower == 'М':
+        sex = 'Мужчина'
+    elif msg.text.lower == 'Ж':
+        sex = 'Женщина'
+    bot.send_message(msg.chat.id, f'''Регистрация завершена. Твой персонаж {name}, пол - {sex})
 
 
 @server.route('/' + TOKEN, methods=['POST'])
